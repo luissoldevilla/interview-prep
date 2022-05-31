574b38e77598104ec00e2899b34971126ee8699c
luissoldevilla <luis.angelsoto@hotmail.com>
2022-05-30 21:38:38 -0400

    Binary Tree constructor:   develop insert method

--- Binary Trees/bst-construction.js
+++ Binary Trees/bst-construction.js
@@ -9,9 +9,29 @@ class BST {
     this.right = null;
   }
 
+  //Average: O(log(n)) Time | O(1) Space
+  // Worst: O(n) Time | O(1) Space
   insert(value) {
     // Write your code here.
     // Do not edit the return statement of this method.
+    let currentNode = this;
+    while (true) {
+      if (value < currentNode.value) {
+        if (currentNode.left === null) {
+          currentNode.left = new BST(value);
+          break;
+        } else {
+          currentNode = currentNode.left;
+        }
+      } else {
+        if (currentNode.right === null) {
+          currentNode.right = new BST(value);
+          break;
+        } else {
+          currentNode = currentNode.right;
+        }
+      }
+    }
     return this;
   }
 

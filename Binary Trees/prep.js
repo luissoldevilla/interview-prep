d18386587394bb3da1a9e82dc2037b2de410f135
luissoldevilla <luis.angelsoto@hotmail.com>
2022-06-07 15:38:43 -0400

    Commit message:
    Worked on objects

--- Binary Trees/prep.js
+++ Binary Trees/prep.js
@@ -4,3 +4,20 @@ const person = {
     name: 'Angel',
     course: 'Front-end',
 };
+
+console.log(person); 
+console.log(typeof JSON.stringify(person));
+// console.log(typeof JSON.stringify(person));
+
+const map = new Map();
+map.set(123, 'hello');
+
+console.log(map.get(123));
+
+const set = new Set();
+set.add('hello');
+set.add('world');
+console.log(set.has('hello'));
+console.log(set.size);
+console.log(set);
+console.log('')

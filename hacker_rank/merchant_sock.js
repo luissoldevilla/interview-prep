9d0bef35e0f99cebb59339d6e4ee057d73feac01
luissoldevilla <luis.angelsoto@hotmail.com>
2022-06-16 17:03:08 -0400

    Added two more solutions for the socks

--- hacker_rank/merchant_sock.js
+++ hacker_rank/merchant_sock.js
@@ -29,6 +29,7 @@ function sockMerchant(n, ar) {
     
     for (let element of ar) {
         socks[element] = socks[element] + 1 || 1;
+        // console.log(socks);
         if (socks[element] % 2 ===0) {
             pairs += 1;
         }
@@ -41,3 +42,40 @@ sockMerchant(10, [10, 20, 20, 10, 10, 30, 50, 10, 20]);
 sockMerchant(8, [10, 20, 20, 10, 10, 30, 50]);
 sockMerchant(12, [10, 20, 20, 10, 10, 30, 50, 50, 50, 60, 60]);
 
+function sockMerchant2(n, ar) {
+    // Write your code here
+    const uniqueValues = [... new Set(ar)];
+    let count = 0;
+
+    uniqueValues.forEach(value => {
+        const filterValues = ar.filter(element => element == value)
+        const pairsNumber = Math.floor(filterValues.length/2)
+        count += pairsNumber
+    })
+    console.log(count);
+    return count
+}
+
+
+sockMerchant2(10, [10, 20, 20, 10, 10, 30, 50, 10, 20]);
+sockMerchant2(8, [10, 20, 20, 10, 10, 30, 50]);
+sockMerchant2(12, [10, 20, 20, 10, 10, 30, 50, 50, 50, 60, 60]);
+
+function sockMerchant3(n, ar) {
+    let sockMap = {}
+    let result = 0
+    for (let sock of ar) {
+        sockMap[sock] ? sockMap[sock]++ : sockMap[sock] = 1
+    }
+    for (let sock in sockMap) {
+        if (sockMap[sock] > 1) {
+            result = result + Math.floor(sockMap[sock] / 2)
+        }
+    }
+    console.log('este el la repuesta ' + result);
+    return result
+}
+
+sockMerchant3(10, [10, 20, 20, 10, 10, 30, 50, 10, 20]);
+sockMerchant3(8, [10, 20, 20, 10, 10, 30, 50]);
+sockMerchant3(12, [10, 20, 20, 10, 10, 30, 50, 50, 50, 60, 60]);

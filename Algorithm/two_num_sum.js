89954666e37d9682bd6a737b33ec8761fc4f54fa
luissoldevilla <luis.angelsoto@hotmail.com>
2022-06-16 08:41:57 -0400

    Made second solution for two_num_sum

--- Algorithm/two_num_sum.js
+++ Algorithm/two_num_sum.js
@@ -1,4 +1,19 @@
+/*
+Write a function that takes in a non-empty array of distinct integers and an integer 
+representing a target sum. If any two numbers in the input array sum up to the target 
+sum, the function should return them in an array, in any order. 
+If no two numbers sum up to the target sum, the function should return an empty array.
+
+Note that the target sum has to be obtained by summing two different integers in the 
+array; you can't add a single integer to itself in order to obtain the target sum.
+
+You can assume that there will be at most one pair of numbers summing up to the target sum.
+
+*/
+
 // First Solution
+
+// O(n^2) time | O(1) space
 
 function twoNumberSum(array, targetSum) {
 
@@ -20,7 +35,7 @@ function twoNumberSum(array, targetSum) {
 
             if (firstNum + secondNum === targetSum) {
                 const answer = [firstNum, secondNum];
-                console.log(answer);
+                console.log('This is the first answer ' + answer);
             }   
         }
     }
@@ -28,3 +43,28 @@ function twoNumberSum(array, targetSum) {
 }
 
 twoNumberSum();
+
+// Second Solution
+
+// O(n) time | O(n) space
+
+function secondSolution(array, targetSum) {
+
+    array = [3, 5, -4, 8, 11, 1, -1, 6];
+    targetSum = 10;
+
+    const nums = {};
+    for (const num of array) {
+        const suma = targetSum - num;
+        if (suma in nums) {
+            const answer2 = [suma, num];
+            console.log('This is the second answer ' + answer2);
+            // return [suma, num];
+        } else {
+            nums [num] = true;
+        }
+    }
+    return [];
+}
+
+secondSolution();

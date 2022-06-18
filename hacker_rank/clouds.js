ee51dd72b1ef69389ed8755b099055a01bde00cb
luissoldevilla <luis.angelsoto@hotmail.com>
2022-06-18 10:48:04 -0400

    clouds problems added a new solutions

--- hacker_rank/clouds.js
+++ hacker_rank/clouds.js
@@ -19,6 +19,28 @@ function looping(d) {
 
 // looping([1, 2, 2, 2, 34]);
 
+// function jumpingOnClouds(c) {
+//     let minJumps = 0;
+//     let currPos = 0;
+//     for(let i = 0; i < c.length; i++) {
+//     const oneJump = c[currPos + 1];
+//     const twoJump = c[currPos + 2];
+    
+//     if(twoJump === 0) {
+//     currPos += 2;
+//     minJumps++;
+//     } else if(oneJump === 0) {
+//     currPos += 1;
+//     minJumps++;
+//     }
+    
+//     }
+//     console.log(minJumps);
+//     return minJumps;
+//     }
+
+    jumpingOnClouds([0, 0, 1, 0, 0, 1, 0,]);
+
 function jumpingOnClouds2(c) {
     // Write your code here
     let a = 0;

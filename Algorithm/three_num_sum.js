d288884b84f2f2875baa486dd770a68f02a7191e
luissoldevilla <luis.angelsoto@hotmail.com>
2022-06-21 09:09:23 -0400

    Algorithm based on the three num sum

---
+++ Algorithm/three_num_sum.js
@@ -0,0 +1,23 @@
+function threeNumberSum(array, targetSum) {
+    // Write your code here.
+  
+    for (let i = 0; i < array.length - 1; i++) {
+      const firstNum = array[i];
+      for (let j = i + 1; j < array.length - 1; j++) {
+        const secondNum = array[j];
+        // console.log(secondNum);
+        for (let k = j + 1; k < array.length -1; k++) {
+          const thirdNum = array[k];
+          if (firstNum + secondNum + thirdNum === targetSum) {
+            // console.log([firstNum, secondNum, thirdNum]);
+            const newArr = [firstNum, secondNum, thirdNum];
+            console.log(newArr.sort((a, b) => a - b));
+
+            return newArr.sort((a, b) => a - b);
+          }
+        }
+      }
+    }
+  }
+
+threeNumberSum([12, 3, 1, 2, -6, 5, -8, 6], 0);

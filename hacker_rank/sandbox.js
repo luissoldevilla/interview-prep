79f100fe17280e50b21b28e6214178cde0f08e12
luissoldevilla <luis.angelsoto@hotmail.com>
2022-06-26 20:59:28 -0400

    Very Big Sum - Review

---
+++ hacker_rank/sandbox.js
@@ -0,0 +1,5 @@
+function findMedian(arr) { 
+    return arr[Math.floor(arr.length/2)]
+}
+
+console.log(findMedian([1,2,4,5]));
---
+++ hacker_rank/veryBigSum.js
@@ -0,0 +1,18 @@
+function aVeryBigSum(ar) {
+    // Write your code here
+
+    // we assigned a variable to keep track of counter
+    let total = 0; 
+    // we make a for that go through the array
+    for (let i = 0; i < ar.length; i++) {
+        // Inside we assign the sum of each element on the array
+       total += ar[i];
+    }
+    // WE RETURNNNN THE VALUE HERE
+    return total;
+
+}
+
+console.log(aVeryBigSum([1, 2, 3, 4, 5]));
+console.log(aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005]));
+

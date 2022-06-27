d69e61fa1e6f269506b41d29654ab7187d2f38b2
luissoldevilla <luis.angelsoto@hotmail.com>
2022-06-27 17:11:21 -0400

    a very big sum review

--- hacker_rank/veryBigSum.js
+++ hacker_rank/veryBigSum.js
@@ -15,4 +15,5 @@ function aVeryBigSum(ar) {
 
 console.log(aVeryBigSum([1, 2, 3, 4, 5]));
 console.log(aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005]));
+console.log(aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005]));
 

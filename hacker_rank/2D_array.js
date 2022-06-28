f6b676434ab2d2b6236e6396145e2af790a16d19
luissoldevilla <luis.angelsoto@hotmail.com>
2022-06-28 08:46:48 -0400

    2D array Hackerrank

---
+++ hacker_rank/2D_array.js
@@ -0,0 +1,34 @@
+
+
+function hourglassSum(arr) {
+    let max = -63;
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            let sum = 0
+            sum = (
+                   arr[i][j] + 
+                   arr[i][j + 1] + 
+                   arr[i][j + 2] + 
+                   arr[i + 1][j + 1] + 
+                   arr[i + 2][j] + 
+                   arr[i + 2][j + 1] + 
+                   arr[i + 2][j + 2]
+                   );
+
+            max = max < sum ? sum : max
+            // console.log(max);
+        }
+    }
+    console.log(max);
+    return max
+}
+
+hourglassSum([
+        [1, 1, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [1, 1, 1, 0, 0, 0],
+        [0, 0, 2, 4, 4, 0],
+        [0, 0, 0, 2, 0, 0],
+        [0, 0, 1, 2, 4, 0]
+     ]);
+

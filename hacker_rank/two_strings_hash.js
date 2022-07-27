460ec88ecd8fea8de0d3e10537b935bf92458db4
luissoldevilla <luis.angelsoto@hotmail.com>
2022-07-27 13:51:20 -0400

    two string problem from hackerrank pt1

---
+++ hacker_rank/two_strings_hash.js
@@ -0,0 +1,26 @@
+function twoStrings(s1, s2) {
+    // Write your code here
+
+    let mainSubstring = {};
+
+    for (let i = 0; i < s1.length; i++) {
+        const substring = s1[i];
+        mainSubstring[substring] = true // ? mainSubstring[substring] + 1 : 1;
+        // console.log(mainSubstring);
+        // console.log(substring); 
+    }
+    for (let j = 0; j < s2.length; j++) {
+        const substring2 = s2[j];
+        if (mainSubstring[substring2]) {
+            console.log('YES');
+            return true;
+        }
+    }
+    console.log('NO')
+    return false;
+
+}
+
+twoStrings('hello', 'world');
+twoStrings('and', 'art');
+twoStrings('and', 'ert');

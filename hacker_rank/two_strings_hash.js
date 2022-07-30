0e3300ea8bc718d34aba2a73e05653ecf6909310
luissoldevilla <luis.angelsoto@hotmail.com>
2022-07-30 00:21:51 -0400

    hashmapping recap

--- hacker_rank/two_strings_hash.js
+++ hacker_rank/two_strings_hash.js
@@ -4,7 +4,9 @@ function twoStrings(s1, s2) {
     // first we declare the our hashmap
     let mainSubstring = {};
 
+    // we do a for loop 
     for (let i = 0; i < s1.length; i++) {
+        // we declare our i into a variable
         const substring = s1[i];
         mainSubstring[substring] = true // ? mainSubstring[substring] + 1 : 1;
         // console.log(mainSubstring);

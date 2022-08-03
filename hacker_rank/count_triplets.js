f7d4cfc545a9eba3a568eecf9d5e1df6dfdc71ee
luissoldevilla <luis.angelsoto@hotmail.com>
2022-08-03 16:31:54 -0400

    count triplets: Curious idea we found we can use two hashmaps!

--- hacker_rank/count_triplets.js
+++ hacker_rank/count_triplets.js
@@ -1,21 +1,36 @@
 function countTriplets(arr, r) {
-    
+    //
     // we declare hashmaps
-    let indices = {};
-    let counter = 0;
+    // let indices = {};
+    // let counter = 0;
 
-    for (let i = 0; i < arr.length; i++) {
-        const indice = arr[i];
-        indices[indice] = true;
-        console.log(indices);
-        if (indices[indice] % r) {// && indice[i+1] == indice[i]+1 && indice[i+2] == indice[i]+2) {
-            console.log(indices[indice]);
-            const cuenta = counter++;
-            console.log(cuenta);
+    // for (let i = 0; i < arr.length; i++) {
+    //     const indice = arr[i];
+    //     indices[indice] = true;
+    //     console.log(indices);
+    //     if (indices[indice] % r) {
+    //         console.log(indices[indice]);
+    //         const cuenta = counter++;
+    //         console.log(cuenta);
 
-        }
+    //     }
+    // }
+    const hGram = {}
+    const hGram2 = {}
+    let count = 0
+    if (arr.length < 3) return 0
+    for (let i = arr.length - 1; i >=0; i--) {
+      let t1 = arr[i]
+      let t2 = t1 * r
+      let t3 = t2 * r
+      count += hGram2[t3] || 0
+      hGram2[t2] ?
+        hGram2[t2] += hGram[t2] || 0 :
+          hGram2[t2] = hGram[t2] || 0
+      hGram[t1] ? hGram[t1]++ : hGram[t1] = 1
     }
-
+    console.log(count);
+    return count
 
 }
 

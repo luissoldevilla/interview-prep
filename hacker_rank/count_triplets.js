60fb7726738b111a336b01512743d00f6e0f85f6
luissoldevilla <luis.angelsoto@hotmail.com>
2022-08-04 17:36:02 -0400

    error handeling fro arrays that are less than 3 elements

--- hacker_rank/count_triplets.js
+++ hacker_rank/count_triplets.js
@@ -1,20 +1,4 @@
 function countTriplets(arr, r) {
-    //
-    // we declare hashmaps
-    // let indices = {};
-    // let counter = 0;
-
-    // for (let i = 0; i < arr.length; i++) {
-    //     const indice = arr[i];
-    //     indices[indice] = true;
-    //     console.log(indices);
-    //     if (indices[indice] % r) {
-    //         console.log(indices[indice]);
-    //         const cuenta = counter++;
-    //         console.log(cuenta);
-
-    //     }
-    // }
 
     // we declare 2 hashmaps
     const hGram = {}

4c1e416a554c5591f27ae3f9586e13a8f0738819
luissoldevilla <luis.angelsoto@hotmail.com>
2022-08-09 16:31:08 -0400

    washmap integration started

--- hacker_rank/frequency_queries.js
+++ hacker_rank/frequency_queries.js
@@ -1,27 +1,30 @@
 function freqQuery(queries) {
     // write your code here
-    let store = [];
-    let output = [];
-    // we decare hashmap
-    let obj = {};
+
+    // declare 2 empty arrays
+    let ops = []; // operations 
+    let output = []; // for the result 
+    // we decare hashmap for populate the arrays
+    let obj = {}; 
+    // varible to check for frequencies starts false.
     let checkFreq = false;
   
     for (let i = 0; i < queries.length; i++) {
       let query = queries[i];
   
       if (query[0] === 1) {
-        store.push(query[1]);
+        ops.push(query[1]);
       } else if (query[0] === 2) {
-        let index = store.indexOf(query[1]);
+        let index = ops.indexOf(query[1]);
         if (index > -1) {
-          store.splice(index, 1);
+          ops.splice(index, 1);
         }
       } else {
         let freq = query[1];
-        if (store.length === 0) {
+        if (ops.length === 0) {
           output.push(0);
         } else {
-          obj = charToObj(store);
+          obj = charToObj(ops);
           for (let number in obj) {
             if (obj[number] === freq) {
               checkFreq = true;

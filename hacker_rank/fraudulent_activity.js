5f5d07a3f53c84a5b121ff2ae7163f277bc5bd61
luissoldevilla <luis.angelsoto@hotmail.com>
2022-08-16 19:06:46 -0400

    We got the first values to got through the array -currntly a bit stuck

--- hacker_rank/fraudulent_activity.js
+++ hacker_rank/fraudulent_activity.js
@@ -1,5 +1,18 @@
 function activityNotifications(expenditure, d) {
     // Write your code here
+    // expenditure.sort((a, b) => a - b);
+
+    // console.log(expenditure);
+    let notifications = 0;
+    let sub = [];
+
+    for (let i = d; i < expenditure.length; i++) {  
+        let gastoActual = expenditure[i];
+        // console.log(gastoActual);
+        for (let j = 0; j < expenditure.length; j++) {
+            console.log(expenditure[j])
+        }
+    }
 
 }
 

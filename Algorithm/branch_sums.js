c41d0a55255e9a6b206e2ebf6c00d4c7c30dba14
luissoldevilla <luis.angelsoto@hotmail.com>
2022-08-23 23:50:17 -0400

    branch sums problem from algoexpert resolve

---
+++ Algorithm/branch_sums.js
@@ -0,0 +1,31 @@
+class BinaryTree {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
+  console.log(BinaryTree);
+  
+  function branchSums(root) { 
+    // Write your code here.
+    const sums = [];
+    calculateBranchSums(root, 0, sums);
+    return sums;
+  }
+  
+  
+  function calculateBranchSums(node, runningSum, sums) {
+    if (!node) return;
+  
+    const newRunningSum = runningSum + node.value;
+    
+    if (!node.left && !node.right) {
+      sums.push(newRunningSum);
+      return;
+    }
+    
+
+    calculateBranchSums(node.left, newRunningSum, sums);
+    calculateBranchSums(node.right, newRunningSum, sums);
+  }

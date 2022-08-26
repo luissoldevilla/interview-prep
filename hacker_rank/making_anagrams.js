e914f4b3751b2a5758334ddf0bbaf1de4aa25cef
luissoldevilla <luis.angelsoto@hotmail.com>
2022-08-26 19:04:42 -0400

    Angaram solution pt1

--- hacker_rank/making_anagrams.js
+++ hacker_rank/making_anagrams.js
@@ -1,9 +1,14 @@
 function makeAnagram(a, b) {
     // Write your code here
+    // declare a hashmap and two counter 
     let map = {}, aCount = 0, bCount = 0;
+
+    // do loop for a
     for (let i = 0; i < a.length; i++) {
+        // we populate our hash
         if (!map[a[i]]) {
             map[a[i]] = 1;
+            // console.log(map[a[i]]);
         } else {
             map[a[i]] += 1;
         }
@@ -13,9 +18,11 @@ function makeAnagram(a, b) {
     for (let j = 0; j < b.length; j++) {
         if (map[b[j]]) {
             aCount += 1;
+            // console.log(aCount);
             map[b[j]] -= 1;
         } else {
             bCount += 1
+            // console.log(bCount);
         }
     }
     console.log((a.length - aCount) + bCount);

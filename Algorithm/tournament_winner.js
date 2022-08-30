4092aa6832374e3ed8cf2e31f98c5a7ca9d7e77a
luissoldevilla <luis.angelsoto@hotmail.com>
2022-08-30 01:39:42 -0400

    algoexpert tournament winner pt1

---
+++ Algorithm/tournament_winner.js
@@ -0,0 +1,29 @@
+
+
+const HOME_TEAM_WON = 1;
+
+function tournamentWinner(competitions, results) {
+  // Write your code here.
+  let currentBestTeam = '';
+  const scores = {[currentBestTeam]: 0};
+
+  for (let idx = 0; idx < competitions.length; idx++) {
+    const result = results[idx];
+    const [homeTeam, awayTeam] = competitions[idx];
+
+    const winningTeam = result === HOME_TEAM_WON ? homeTeam : awayTeam;
+
+    updatedScores(winningTeam, 3, scores);
+
+    if (scores[winningTeam] > scores[currentBestTeam]) {
+      currentBestTeam = winningTeam;
+    }
+  }
+  return currentBestTeam;
+}
+
+function updatedScores(team, points, scores) {
+  if (!(team in scores)) scores[team] = 0;
+
+  scores[team] += points;
+}

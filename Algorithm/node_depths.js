10e8990eda34211a6f5f7faa066d2e51aac27296
luissoldevilla <luis.angelsoto@hotmail.com>
2022-09-03 01:58:11 -0400

    algoexpert node depths setup problems

---
+++ Algorithm/node_depths.js
@@ -0,0 +1,12 @@
+function nodeDepths(root) {
+
+  }
+  
+  // This is the class of the input binary tree.
+  class BinaryTree {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }

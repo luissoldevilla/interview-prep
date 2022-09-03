49ea05b9e26f21baa1a1dc48c895cb803c6837c5
luissoldevilla <luis.angelsoto@hotmail.com>
2022-09-03 01:59:05 -0400

    algoexpert node depths - solving main function

--- Algorithm/node_depths.js
+++ Algorithm/node_depths.js
@@ -1,5 +1,15 @@
 function nodeDepths(root) {
-
+    // Write your code here.
+    let suma = 0;
+    const stack = [{node: root, depth: 0}];
+    while (stack.length > 0) {
+      const {node, depth} = stack.pop();
+      if (node === null) continue;
+      suma += depth;
+      stack.push({node: node.left, depth:depth + 1});
+      stack.push({node: node.right, depth:depth + 1});
+    }
+    return suma
   }
   
   // This is the class of the input binary tree.

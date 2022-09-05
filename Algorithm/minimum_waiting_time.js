26b0967cc6e7816af6c83f4c6e4f5131a7d11922
luissoldevilla <luis.angelsoto@hotmail.com>
2022-09-05 02:06:14 -0400

    Minium waiting time - problem set up

---
+++ Algorithm/minimum_waiting_time.js
@@ -0,0 +1,8 @@
+function minimumWaitingTime(queries) {
+    // Write your code here.
+    queries.sort((a, b) => a - b);
+    console.log(queries);
+    
+    return 0;
+  }
+minimumWaitingTime([3,2,1,2,6]);

70073d30393e1d9faf3dc82aafec8610d1f35188
luissoldevilla <luis.angelsoto@hotmail.com>
2022-09-06 11:49:32 -0400

    Sorted in ascending order

--- Algorithm/minimum_waiting_time.js
+++ Algorithm/minimum_waiting_time.js
@@ -1,8 +1,13 @@
 function minimumWaitingTime(queries) {
     // Write your code here.
+    let counter = 0;
+
     queries.sort((a, b) => a - b);
-    console.log(queries);
     
+    console.log(queries);
+    let totaWaitingTime = 0;
+
+
     return 0;
   }
 minimumWaitingTime([3,2,1,2,6]);

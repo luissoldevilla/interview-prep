ff609f64c5fef592186767707f492c69f932567e
luissoldevilla <luis.angelsoto@hotmail.com>
2022-09-11 19:38:56 -0400

    checking forsorted arrays

--- Algorithm/class_photos.js
+++ Algorithm/class_photos.js
@@ -2,8 +2,10 @@ function classPhotos(redShirtHeights, blueShirtHeights) {
     // Write your code here.
     // we sort the arrays first
     redShirtHeights.sort((a,b) => b - a);
+    console.log(redShirtHeights);
     blueShirtHeights.sort((a,b) => b - a);
+    console.log(blueShirtHeights);
     return false;
 }
 
-classPhotos();
+classPhotos([5, 8, 1, 3, 4],[6, 9, 2, 4, 5]);

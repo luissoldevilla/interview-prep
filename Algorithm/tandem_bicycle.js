a8c4f516c794dbfe7d4cc1bfe710f35ef9a8a313
luissoldevilla <luis.angelsoto@hotmail.com>
2022-09-13 17:28:23 -0400

    Problem Set up 3

--- Algorithm/tandem_bicycle.js
+++ Algorithm/tandem_bicycle.js
@@ -3,7 +3,4 @@ function tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest) {
     return 0;
 }
 
-tandemBicycle();
-"redShirtSpeeds": [5, 5, 3, 9, 2],
-"blueShirtSpeeds": [3, 6, 7, 2, 1],
-"fastest": true
+tandemBicycle([5, 5, 3, 9, 2], [3, 6, 7, 2, 1], true);

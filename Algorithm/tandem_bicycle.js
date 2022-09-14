168c5f05528c09f755c970dd84db92ed3d057fb6
luissoldevilla <luis.angelsoto@hotmail.com>
2022-09-14 18:13:56 -0400

    Completes secondary function

--- Algorithm/tandem_bicycle.js
+++ Algorithm/tandem_bicycle.js
@@ -8,7 +8,13 @@ function tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest) {
     function reverseArrayInPlace(array) {
         let start = 0;
         let end = array.length - 1;
-
+        while (start < end) {
+            const temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+            start++;
+            end--;
+          }
       }
 }
 

ee5807b546482a26cc67e6b9fcb3e85b5f0010df
luissoldevilla <luis.angelsoto@hotmail.com>
2022-09-14 18:12:05 -0400

    Made a secondary function to reverse the array

--- Algorithm/tandem_bicycle.js
+++ Algorithm/tandem_bicycle.js
@@ -3,8 +3,13 @@ function tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest) {
     console.log(redShirtSpeeds);
     console.log(blueShirtSpeeds)
     console.log(fastest);
-    
+
     return 0;
+    function reverseArrayInPlace(array) {
+        let start = 0;
+        let end = array.length - 1;
+
+      }
 }
 
 tandemBicycle([5, 5, 3, 9, 2], [3, 6, 7, 2, 1], true);

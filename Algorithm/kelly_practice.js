051a91cad4a0f563eb290675c3d7ef5257fe2fdb
luissoldevilla <luis.angelsoto@hotmail.com>
2022-10-15 22:12:58 -0400

    Kelly Practice

---
+++ Algorithm/kelly_practice.js
@@ -0,0 +1,15 @@
+let world = 50;
+
+function isString(a,b,c) {
+    let list = [a,b,c]
+    for (let i = 0; i < list.length; i++){
+        console.log(typeof(list[i]))
+        if (typeof(list[i]) === "") {
+            console.log("strings") 
+        } else console.log("not strings")
+    }
+    // console.log(list)
+    return list
+}
+
+isString("hi","kelly", world);

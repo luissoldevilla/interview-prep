674865ed66c8c63cb4a1c1403df30b2a5d786561
luissoldevilla <luis.angelsoto@hotmail.com>
2022-10-26 23:49:13 -0400

    Tandem Bycicle: if fastest is false handle

--- Algorithm/tandem_bicycle.js
+++ Algorithm/tandem_bicycle.js
@@ -1,15 +1,18 @@
 function tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest) {
     // Write your code here.
+    
+    // we sort all the bycicles speeds[arrays]
+    redShirtSpeeds.sort((a,b) => a - b);
+    blueShirtSpeeds.sort((a,b) => a -b);
+    
     console.log(redShirtSpeeds);
     console.log(blueShirtSpeeds)
     console.log(fastest);
 
-    // we sort all the bycicles speeds[arrays]
-    redShirtSpeeds.sort((a,b) => a - b);
-    blueShirtSpeeds.sort((a,b) => a -b);
-  
+    if (!fastest) reverseArrayInPlace(redShirtSpeeds);
 
     return 0;
+
     function reverseArrayInPlace(array) {
         let start = 0;
         let end = array.length - 1;

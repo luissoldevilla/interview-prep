ef0596fd7db2dac8bf0ddd5929404e2d27430abf
luissoldevilla <luis.angelsoto@hotmail.com>
2022-11-09 16:24:53 -0500

    Two Number Sum: Fisrt Index Implemented

--- AlgoExpert/two_number.js
+++ AlgoExpert/two_number.js
@@ -1,7 +1,9 @@
-// 
+// Find which 2 integers in the array add up to to the targetSum
+// Return those 2 intergers in a array
+
 function twoNumberSum(array, targetSum) {
     for (let i = 0; i < array.lenght; i++){
-
+        const firstNum = array[i]
     }
 }
 

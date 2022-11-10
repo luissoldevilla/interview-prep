c5b6ce3041c04340ca52e6d765650bb851812f95
luissoldevilla <luis.angelsoto@hotmail.com>
2022-11-10 17:08:14 -0500

    Two Number Sum: If Statement Implemented

--- AlgoExpert/two_number.js
+++ AlgoExpert/two_number.js
@@ -1,6 +1,8 @@
 // Find which 2 integers in the array add up to to the targetSum
 // Return those 2 intergers in a array
 
+// This is the First Solution
+
 function twoNumberSum(array, targetSum) {
     for (let i = 0; i < array.length - 1; i++){
         const firstNum = array[i]

c4bdd5a98752e4d2cc60e6eb0b5d95fb9609c4fc
luissoldevilla <luis.angelsoto@hotmail.com>
2022-11-17 23:02:31 -0500

    Validate Subsequence: Set up While Loop

--- AlgoExpert/validate_subsequence.js
+++ AlgoExpert/validate_subsequence.js
@@ -4,6 +4,10 @@ function isValidSubsequence(array, sequence) {
     // we set up counters
     let arr = 0;
     let sub = 0;
+    // we set up a  while loop 
+    while (arr < array.length && sub < sequence.length) {
+
+    }
 }
 
 isValidSubsequence()

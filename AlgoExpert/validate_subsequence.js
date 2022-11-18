41cb9d75bd040caf8d75ac82d9e0cefc713657b8
luissoldevilla <luis.angelsoto@hotmail.com>
2022-11-17 23:05:29 -0500

    Validate Subsequence: Set up If statement

--- AlgoExpert/validate_subsequence.js
+++ AlgoExpert/validate_subsequence.js
@@ -6,8 +6,8 @@ function isValidSubsequence(array, sequence) {
     let sub = 0;
     // we set up a  while loop 
     while (arr < array.length && sub < sequence.length) {
-
+        console.log(array[arr])
     }
 }
 
-isValidSubsequence()
+isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10])

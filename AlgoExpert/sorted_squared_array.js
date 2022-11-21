764cd80fe0746370b61668344a6b1a0e10b41551
luissoldevilla <luis.angelsoto@hotmail.com>
2022-11-21 15:24:27 -0500

    Sorted Square Array

--- AlgoExpert/sorted_squared_array.js
+++ AlgoExpert/sorted_squared_array.js
@@ -2,6 +2,7 @@ function sortedSquaredArray(array   ) {
     const sortedSquares = new Array(array.length).fill(0); 
     for (let i = 0; i < array.length; i++) {
         const int = array[i];
+        sortedSquares[i] = int * int;
     }
 
 }

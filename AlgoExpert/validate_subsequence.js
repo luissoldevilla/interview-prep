697636aa88be4d7fa13dceca69cfa4ce8dcff2bc
luissoldevilla <luis.angelsoto@hotmail.com>
2023-01-04 13:10:42 -0500

    Validate problem

--- AlgoExpert/validate_subsequence.js
+++ AlgoExpert/validate_subsequence.js
@@ -26,6 +26,7 @@ function isValidSubsequence(array, sequence) {
 
 isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10])
 isValidSubsequence([1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10])
+isValidSubsequence([1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10])
 
 
 // Second solution

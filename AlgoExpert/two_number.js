151601d16f78c05a84b1d89523a16bc724bbf819
luissoldevilla <luis.angelsoto@hotmail.com>
2023-01-08 19:10:19 -0500

    Testing by Calling different inputs

--- AlgoExpert/two_number.js
+++ AlgoExpert/two_number.js
@@ -29,6 +29,7 @@ twoNumberSum([1, 2, 3, 4, 5, 6, 7, 8, 9, 15], 18)
 twoNumberSum([-7, -5, -3, -1, 0, 1, 3, 5, 7], -5)
 twoNumberSum([-7, -5, -3, -1, 0, 1, 3, 5, 7], -5)
 twoNumberSum([-7, -5, -3, -1, 0, 1, 3, 5, 7], -5)
+twoNumberSum([-7, -5, -3, -1, 0, 1, 3, 5, 7], -5)
 
 
 // Second Solution 

f99415d9120b5516867f8225edfeffce3e160b5c
luissoldevilla <luis.angelsoto@hotmail.com>
2023-01-20 11:32:30 -0500

    Validation: testing

--- AlgoExpert/validate_subsequence.js
+++ AlgoExpert/validate_subsequence.js
@@ -37,6 +37,7 @@ function isValidSubsequence2(array, sequence) {
     // Set up a for loop that iterates through array 
     //  while declaring a constant named value
     for (const value of array) {
+        console.log(value)
         if (seqIdx === sequence.length) break;
         if (sequence[seqIdx] === value) {
             seqIdx++;

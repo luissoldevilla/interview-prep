19e764c2a5b15674d7dd63a09fb64af71a89bbcc
luissoldevilla <luis.angelsoto@hotmail.com>
2023-01-23 22:08:14 -0500

    Validation: testing

--- AlgoExpert/validate_subsequence.js
+++ AlgoExpert/validate_subsequence.js
@@ -47,6 +47,7 @@ function isValidSubsequence2(array, sequence) {
     console.log(seqIdx)
     console.log(seqIdx)
     console.log(seqIdx)
+    console.log(seqIdx)
     return seqIdx === sequence.length;
 }
 

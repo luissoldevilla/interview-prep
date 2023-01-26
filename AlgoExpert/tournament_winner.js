a080f87debf969d9f46850c4a5918ea3d94756df
luissoldevilla <luis.angelsoto@hotmail.com>
2023-01-26 16:09:38 -0500

    Problem: Tournament Winner

--- AlgoExpert/tournament_winner.js
+++ AlgoExpert/tournament_winner.js
@@ -2,4 +2,4 @@ function tournamentWinner(competitions, results) {
 
 }
 
-tournamentWinner([["HTML", "Java"], ["Java", "Python"], ["Python", "HTML"]])
+tournamentWinner([["HTML", "Java"], ["Java", "Python"], ["Python", "HTML"]], [0, 1, 1])

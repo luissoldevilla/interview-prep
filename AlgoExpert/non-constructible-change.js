cc310c6c633673cb632c4a44047dc576124d59a6
luissoldevilla <luis.angelsoto@hotmail.com>
2023-02-01 12:47:46 -0500

    Non constructible change: Sorting

--- AlgoExpert/non-constructible-change.js
+++ AlgoExpert/non-constructible-change.js
@@ -2,6 +2,8 @@ function nonConstructibleChange(coins) {
     console.log(coins);
     coins.sort((a,b) => a - b);
     console.log(coins.sort((a,b) => a - b))
+
+    let currentChangeCreated = 0;
 }
 
 nonConstructibleChange([5, 7, 1, 1, 2, 3, 22])

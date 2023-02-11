0136a658ec34f9860fd65d593d40c12d9d9ed78f
luissoldevilla <luis.angelsoto@hotmail.com>
2023-02-11 00:23:07 -0500

    Find Closest Value In BST

--- AlgoExpert/closest_value_bst.js
+++ AlgoExpert/closest_value_bst.js
@@ -6,6 +6,9 @@ function findClosestValueInBst(tree, target) {
 function findClosestValueInBstHelper(tree, target, closest) {
     console.log(tree)
     if (tree === null) return closest
+    if (Math.abs(target - closest) > Math.abs(target - tree.value)) {
+
+    }
 }
 
 findClosestValueInBst(

caad357ff3f1c42315c32e7ed6c1df6a8a7f9f80
luissoldevilla <luis.angelsoto@hotmail.com>
2023-02-19 01:20:11 -0500

    Find Closest Value In BST

--- AlgoExpert/closest_value_bst.js
+++ AlgoExpert/closest_value_bst.js
@@ -66,6 +66,7 @@ findClosestValueInBst(
           "nodes": [
             {"id": "10", "left": "5", "right": "15", "value": 10},
             {"id": "15", "left": "13", "right": "22", "value": 15},
+            {"id": "22", "left": null, "right": null, "value": 22},
           ],
           "root": "10"
         },

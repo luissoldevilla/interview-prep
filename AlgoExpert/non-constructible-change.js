057b935cc8b129b066b999125b41c7e0c88e7f6b
luissoldevilla <luis.angelsoto@hotmail.com>
2023-02-25 06:34:32 -0500

    Console logging

--- AlgoExpert/non-constructible-change.js
+++ AlgoExpert/non-constructible-change.js
@@ -35,4 +35,5 @@ function nonConstructibleChange(coins) {
 // Sample output: 
 // 20
 nonConstructibleChange([5, 7, 1, 1, 2, 3, 22])
+nonConstructibleChange([5, 7, 1, 1, 2, 3, 24])
 nonConstructibleChange([1, 5, 1, 1, 1, 10, 15, 20, 100])

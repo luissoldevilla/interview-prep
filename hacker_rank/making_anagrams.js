a6c14b7711c642c3b7624201229e04a46856ae82
luissoldevilla <luis.angelsoto@hotmail.com>
2023-03-19 11:46:30 -0400

    recap

--- hacker_rank/making_anagrams.js
+++ hacker_rank/making_anagrams.js
@@ -81,10 +81,6 @@ function makeAnagram(a, b) {
   }
 
 makeAnagram('cde', 'abc')
-makeAnagram('cde', 'abc')
-makeAnagram('cde', 'abc')
-makeAnagram('cde', 'abc')
-makeAnagram('cde', 'abc')
 getFrequency('cde', 'abc')
   
   

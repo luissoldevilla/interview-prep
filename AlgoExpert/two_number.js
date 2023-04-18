bb2d5c95d708e9e8c11386e05a93973be2d9de59
luissoldevilla <luis.angelsoto@hotmail.com>
2023-04-18 16:39:57 -0400

    Recap Algo problems

--- AlgoExpert/two_number.js
+++ AlgoExpert/two_number.js
@@ -23,7 +23,7 @@ function twoNumberSum(array, targetSum) {
 }
 
 console.log(twoNumberSum([2,7,11,15], 9))
-// twoNumberSum([1, 2, 3, 4, 5, 6, 7, 8, 9], 17)
+twoNumberSum([1, 2, 3, 4, 5, 6, 7, 8, 9], 17)
 // twoNumberSum([1, 2, 3, 4, 5, 6, 7, 8, 9, 15], 18)
 // twoNumberSum([-7, -5, -3, -1, 0, 1, 3, 5, 7], -5)
 

bb955fc4a05c12fd11294f8ebc7dcdec75f2a06b
luissoldevilla <luis.angelsoto@hotmail.com>
2023-07-04 11:24:58 -0400

    Resetting

--- AlgoExpert/validate_subsequence.js
+++ AlgoExpert/validate_subsequence.js
@@ -70,4 +70,5 @@ isValidSubsequence2([1, 2, 3, 4], [2, 4])
 isValidSubsequence2([1, 2, 3, 4], [2, 4])
 isValidSubsequence2([1, 2, 3, 4], [2, 4])
 isValidSubsequence2([1, 2, 3, 4], [2, 4])
+isValidSubsequence2([1, 2, 3, 4], [2, 4])
 
